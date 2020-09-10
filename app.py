61963bd7eb115dd5f470223988d2675807cab06f
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-09-10 11:22:23 -0600

    Update seeds.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from models import CelestialBodies, Landmark, User, Passenger
 def hello():
     return "App is running"
 
-@app.route('/celestial_bodies')
+@app.route('/api/v1/celestial_bodies', methods = ['GET'])
 def get_all():
     try:
         bodies = CelestialBodies.query.all()
@@ -27,53 +27,78 @@ if __name__ == '__main__':
 
 @app.cli.command('db_seed')
 def db_seed():
-    body1 = CelestialBodies(name='Earth',
-                     image='Motivation',
+    mercury = CelestialBodies(name='Mercury',
+                     image='https://cdn.mos.cms.futurecdn.net/GA4grWEsUYUqH58cDbRBw8.jpg',
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=0.37,
+                     planet_day=58.65,
+                     planet_year=87.96)
 
-    body6 = CelestialBodies(name='Mars',
-                     image='Motivation',
+    venus = CelestialBodies(name='Venus',
+                     image="https://astronomy.com/-/media/Images/News%20and%20Observing/News/2020/04/Venus1__1_.jpg?mw=600",
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=0.90,
+                     planet_day=243.02,
+                     planet_year=224.70)
 
-    body2 = CelestialBodies(name='Venus',
-                     image='Motivation',
+    mars = CelestialBodies(name='Mars',
+                     image='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSY5oCXASYgKXI1MFGmRbgs9WmSnULsnOe_fg&usqp=CAU',
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=0.38
+                     planet_day=1.02,
+                     planet_year=686.98)
 
-    body3 = CelestialBodies(name='Neptune',
-                     image='Motivation',
+    jupiter = CelestialBodies(name='Jupiter',
+                     image='https://3.bp.blogspot.com/-JzB2ruOjBOs/WJy8tR_tJSI/AAAAAAAABdA/26gANOQ4Y4IZyMnEGS2L8X-dvhVhGL0ZQCLcB/s1600/jupiter_HD.jpg',
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=2.53,
+                     planet_day=0.41,
+                     planet_year=4332.59)
 
-    body4 = CelestialBodies(name='Pluto',
-                     image='Motivation',
+    saturn = CelestialBodies(name='Saturn',
+                     image='https://solarsystem.nasa.gov/system/resources/list_images/2490_hubblesaturn_320.png',
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=1.06,
+                     planet_day=0.44,
+                     planet_year=10759.22)
 
-    body5 = CelestialBodies(name='Uranus',
-                     image='Motivation',
+    uranus = CelestialBodies(name='Uranus',
+                     image='https://i2-prod.mirror.co.uk/science/article11370299.ece/ALTERNATES/s615/1_Uranus.jpg',
                      celestial_body_type='Planet',
-                     gravity=4.2,
-                     planet_day=3.4,
-                     planet_year=5.6)
+                     gravity=0.90,
+                     planet_day=0.72,
+                     planet_year=30685.40)
 
-    db.session.add(body1)
-    db.session.add(body2)
-    db.session.add(body3)
-    db.session.add(body4)
-    db.session.add(body5)
+    neptune = CelestialBodies(name='Neptune',
+                     image='https://media.wired.com/photos/5d04045bde1abfe4e801d054/191:100/w_2292,h_1200,c_limit/Science-Neptune-FA-PIA01492_orig.jpg',
+                     celestial_body_type='Planet',
+                     gravity=1.14,
+                     planet_day=0.67,
+                     planet_year=60189.00)
+
+    moon = CelestialBodies(name='Moon',
+                     image='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSA0QNeR2YGmCWvRtZqsYN9Ft44WxYJEArbtw&usqp=CAU',
+                     celestial_body_type='Moon',
+                     gravity=0.16,
+                     planet_day=27.32,
+                     planet_year=null)
+
+    sun = CelestialBodies(name='Sun',
+                     image='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRkKZ8nFulIMZAK8MKI8kzvsfGnaa3YlqMMRA&usqp=CAU',
+                     celestial_body_type='Star',
+                     gravity=27.95,
+                     planet_day=25.38,
+                     planet_year=null)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(mars)
+    db.session.add(jupiter)
+    db.session.add(saturn)
+    db.session.add(uranus)
+    db.session.add(neptune)
+    db.session.add(moon)
+    db.session.add(sun)
     db.session.commit()
     print('Database seeded!')
 
